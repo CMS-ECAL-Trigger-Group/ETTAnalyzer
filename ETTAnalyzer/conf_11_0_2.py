e1da52a990585a6433fc3c005bf134a507125e90
Raman Khurana <raman.khurana@cern.ch>
2020-11-27 14:30:33 +0100

    adding header.txt preparelog.py readlog.py

--- ETTAnalyzer/conf_11_0_2.py
+++ ETTAnalyzer/conf_11_0_2.py
@@ -67,7 +67,7 @@ process.ecalTriggerPrimitiveDigis = cms.EDProducer("EcalTrigPrimProducer",
    BarrelOnly = cms.bool(False),
    Famos = cms.bool(False),
    TcpOutput = cms.bool(False),
-                                                   Debug = cms.bool(True),
+                                                   Debug = cms.bool(False),
    binOfMaximum = cms.int32(6), ## optional from release 200 on, from 1-10
 
 )
@@ -136,8 +136,8 @@ process.TFileService = cms.Service("TFileService",
 
 process.p = cms.Path(process.L1Reco*
                      process.gtStage2Digis*
-                     process.ecalTriggerPrimitiveDigis*
-                     process.tuplizer
+                     process.ecalTriggerPrimitiveDigis
+                     #*process.tuplizer
                  )
 
 
---
+++ ETTAnalyzer/preparelog.py
@@ -0,0 +1,39 @@
+import os 
+
+vetos=["Initiating","Successfully"]
+
+log_in = open('log')
+
+
+log_out = open('amplifier_filter_log.txt', 'w')
+header_ = open('header.txt').readline()
+log_out.write(header_)
+def myjoin(to_join, deli=" "):
+    output=""
+    for i in to_join: 
+        output = output + i + " "
+    return output
+
+pre_str=""
+
+for iline in log_in:
+    vetoline=False
+    for iveto in vetos: 
+        if iveto in iline:
+            vetoline=True
+    if vetoline: continue 
+    if "Closed file file" in iline: break 
+    if "Begin processing" in iline:
+        run_ =   str(iline.rsplit(" ")[6].split(",")[0] )
+        lumi_ =  str(iline.rsplit(" ")[10].split(",")[0] )
+        event_ = str(iline.rsplit(" ")[8].split(",")[0])
+        pre_str = myjoin([run_, lumi_, event_], " ")
+    
+    else: 
+        iline = pre_str + iline
+        log_out.write(iline)
+
+
+    
+
+log_out.close()

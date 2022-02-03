2de926cec496b4f04ed8bb1c281995d1d5657487
Abe Tishelman-Charny <abe2323@yahoo.com>
2022-02-03 22:00:10 +0100

    Shuffled files and directories a bit

--- ETTAnalyzer/weights/weights_encoder.py
+++ ETTAnalyzer/weights/weights_encoder.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np 
+from matplotlib import pyplot as plt 
 
 ''' 
 2 February 2022 
@@ -11,6 +12,16 @@ Original code by William Richard Smith (2019) here: https://gitlab.cern.ch/cms-e
 
 This script simulate the loss of precision in decimal weights given by the encoding. 
 '''
+
+#Encoded weights back to decimals to look at differences
+def encoded_to_decimal(enc_weights_not_corrected):
+    back_weights = [0]*5
+    for count, encw in enumerate(enc_weights_not_corrected):
+        if encw > 63:
+            encw = -(128 -encw)
+        back_weights[count] = encw/64
+    
+    return back_weights
 
 def decimal_to_encoded(weights):
     enc_weights_not_corrected = [0]*5
@@ -29,8 +40,26 @@ def decimal_to_encoded(weights):
     return enc_weights_not_corrected
 
 if (__name__ == '__main__'): 
-	DecimalWeights = [0.265625, -0.71875, 0.0, -0.546875, 1.0] # min_delta = 2.5 
-	EncodedWeights = decimal_to_encoded(DecimalWeights)
-	print("Encoded weights:",EncodedWeights)
-	print("Sum:",np.sum(EncodedWeights)) # [17 82 128 93 64]
+	# DecimalWeights = [0.265625, -0.71875, 0.0, -0.546875, 1.0] # min_delta = 2.5 
+	# EncodedWeights = decimal_to_encoded(DecimalWeights)
+	# print("Encoded weights:",EncodedWeights)
+	# print("Sum:",np.sum(EncodedWeights)) # [17 82 128 93 64]
+
+	encoded_vals = []
+	decimal_vals = []
+
+	for i in range(-400, 400):
+		weight = [int(i)]
+		decimal = encoded_to_decimal(weight)
+		print("Encoded, Decimal: %s, %s"%(weight[0], decimal[0])) 
+		encoded_vals.append(weight[0])
+		decimal_vals.append(decimal[0])
+
+	fig, ax = plt.subplots()
+	plt.plot(encoded_vals, decimal_vals)
+	plt.savefig("weights.png")
+	plt.close()
+
 	print("DONE")
+
+

8dce10ba580e5f3a7c59a608901e74f8dca93a84
Abe Tishelman-Charny <abe2323@yahoo.com>
2022-02-14 10:45:06 +0100

    Removing unused crab configs

--- ETTAnalyzer/CrabConfig_12_1_0_pre3.py
+++ ETTAnalyzer/CrabConfig_12_1_0_pre3.py
@@ -7,17 +7,17 @@ crab submit -c CrabConfig_12_1_0_pre3.py
 """
 
 # Choose dataset to re-emulate:
-Dataset = "PilotBeam2021"
+# Dataset = "PilotBeam2021" # 2021 Pilot Beam double weights runs: Full Readout
+Dataset = "Run2_FR" # 306425: 2017F. 324725: 2018D
 # Dataset = "ZeroBias2018C"
-# Dataset = "Run2_FR"
 
 DatasetInfo = {
-  "PilotBeam2021" : [["346446", "346447"], "Runs_346446_346447_PilotBeam_2021"],
-  "ZeroBias2018C" : [],
-  "Run2_FR" : [["324725", "306425"], "Runs_324725_306425_FullReadoutData"]
+  "PilotBeam2021" : [["346446", "346447"], "Runs_346446_346447_PilotBeam_2021", "120X_dataRun3_HLT_v3"], 
+  "ZeroBias2018C" : [None, None, None],
+  "Run2_FR" : [["324725", "306425"], "Runs_324725_306425_FullReadoutData", "103X_dataRun2_v6"] # 103X_dataRun2_v6 ? 
 }
 
-runs, DatasetLabel = DatasetInfo[Dataset]
+runs, DatasetLabel, UserGlobalTag = DatasetInfo[Dataset]
 
 # Configuration parameters 
 inDir = "/afs/cern.ch/work/a/atishelm/private/CMS-ECAL-Trigger-Group/CMSSW_12_1_0_pre3/src/ETTAnalyzer/ETTAnalyzer/"
@@ -26,7 +26,7 @@ WeightsWP = "2p5Prime"  # odd weights working point. Options: [2p5Prime, 0p5Prim
 ODD_PF = 1 # 0: No ODD peak finder. 1: With ODD peak finder
 addFilePrefix = 0 # Add "file:" to start of file paths 
 removeEOSprefix = 1 
-RecoMethod = "Multifit" # options: Multifit, weights
+RecoMethod = "weights" # options: Multifit, weights
 
 if(ODD_PF): 
   TPMode_file = "EcalTPG_TPMode_Run3_zeroingOddPeakFinder.db"
@@ -46,7 +46,6 @@ print("RecoMethod:",RecoMethod)
 
 CMS_files = []
 print("Adding CMS files...")
-#runs = ["346446", "346447"] # 2021 Pilot Beam double weights runs: Full Readout
 
 # By run number 
 for run in runs:
@@ -62,6 +61,28 @@ for run in runs:
                  file_path = file 
 
              CMS_files.append(file_path)
+
+# To get 2018D ZeroBias data files 
+"""
+Nblocks = 11 # Max: 11 
+##-- By block number 
+for block_i in range(0, Nblocks):
+    print "On block",block_i
+    text_file_path = "RunsAndFiles/Files_Block_%s.txt"%(block_i)
+    with open(text_file_path) as f: 
+        content = f.readlines()
+        content = [x.strip() for x in content] 
+        for file in content:
+            CMS_files.append(file)  
+
+# for computing lumi 
+# https://twiki.cern.ch/twiki/bin/view/CMS/AbrahamTishelmanCharnyHomepage#Checking_Dataset_Luminosity
+# export PATH=$HOME/.local/bin:/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin:$PATH
+# pip install --install-option="--prefix=$HOME/.local" brilws
+# voms 
+##-- brilcalc lumi -r 320038            
+
+"""
 
 if(oneFile):
   CMS_files = [CMS_files[0]] # take first file of files list 
@@ -85,7 +106,7 @@ config.General.transferLogs = False
 # cmssw configuration file parameters 
 config.JobType.pyCfgParams = [
                                 'OverrideWeights=1', # whether or not to override weights from global tag 
-                                'UserGlobalTag=120X_dataRun3_HLT_v3', # global tag 
+                                'UserGlobalTag=%s'%(UserGlobalTag), # global tag 
                                 'TPModeSqliteFile=%s'%(TPMode_file), # strip zeroing, with or without ODD PF configs to try: [EcalTPG_TPMode_Run3_zeroingOddPeakFinder.db, EcalTPG_TPMode_Run3_zeroing,db]
                                 'OddWeightsGroupSqliteFile=OneEBOneEEset_adding2021Strips.db', # weights group for each strip - defines which set of ODD weights each strip should use 
                                 'BarrelOnly=1', # only run over ECAL barrel 
